f3bbbfb7d7b017dc30afa39e405d450885b7e9fc
Pedro Wiskutzki <pedro.wiskutzki@neki-it.com.br>
2023-02-02 16:42:07 -0300

    feat(Post): Add Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,38 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            src="https://avatars.githubusercontent.com/u/110680950?v=4"
+            className={styles.avatar}
+          />
+          <div className={styles.authorInfo}>
+            <strong>Pedro Wiskutzki</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 15:44h" dateTime="2023-01-02 15:13:30">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galera 👋 </p>
+        <p>
+          {" "}
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da{" "}
+        </p>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto </a> <a href="">#nlw</a> <a href="">#rock</a>
+        </p>
+      </div>
+    </article>
+  );
+}
