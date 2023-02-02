316a132518656ed5068f5904710e60de84cff5d9
Pedro Wiskutzki <pedro.wiskutzki@neki-it.com.br>
2023-02-02 12:59:16 -0300

    feat(SideBar):Include SideBar and GlobalStyle

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1675316045979-6fdbf556e400?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/110680950?v=4"
+          alt=""
+        />
+        <strong>Pedro Wiskutzki</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
