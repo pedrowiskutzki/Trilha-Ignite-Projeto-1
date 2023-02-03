41d9766e9f86ecef6f76513fb65cea2fa49c2efb
Pedro Wiskutzki <pedro.wiskutzki@neki-it.com.br>
2023-02-03 08:54:17 -0300

    feat(Post): Complet Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,9 +30,20 @@ export function Post() {
           👉 <a href="">jane.design/doctorcare</a>
         </p>
         <p>
-          <a href="">#novoprojeto </a> <a href="">#nlw</a> <a href="">#rock</a>
+          <a href="">#novoprojeto </a> <a href="">#nlw</a>{" "}
+          <a href="">#rockseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Comentar</button>
+        </footer>
+      </form>
     </article>
   );
 }
