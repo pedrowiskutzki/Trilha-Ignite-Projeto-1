dc08069bac54ba6bbe676a2b0e3023cba3d8c169
Pedro Wiskutzki <pedro.wiskutzki@neki-it.com.br>
2023-02-03 18:52:01 -0300

    feat(Delete): Add Delete

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "../avatar/Avatar";
 import styles from "./Comment.module.css";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/pedrowiskutzki.png" />
@@ -15,7 +19,10 @@ export function Comment({ content }) {
                 Cerca de um hora 1h atrás
               </time>
             </div>
-            <button title="Deletar comentário">
+            <button
+              onClick={() => handleDeleteComment()}
+              title="Deletar comentário"
+            >
               <Trash size={24} />
             </button>
           </header>
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -27,8 +27,23 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
   }
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
+
+    setComments(commentsWithoutDeletedOne);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Esse campo é obrigatório");
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -70,15 +85,25 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type="submit">Publicar</button>
+          <button disabled={newCommentText.length === 0} type="submit">
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment key={comment} content={comment} />;
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
